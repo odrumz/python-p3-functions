e544a114b704ac7650f9eeb0420c81410b816a18
DerrickOngoma <Odrumz64@gmail.com>
2024-05-21 11:45:43 +0300

    functions

--- lib/functions.py
+++ lib/functions.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 
 def greet_programmer():
-    pass
+    print("Hello, programmer!")
 
 def greet(name):
-    pass
+    print(f"Hello, {name}!")
+greet("Naureen")
 
 def greet_with_default(name="programmer"):
-    pass
+    print(f"Hello, {name}!")
+greet_with_default()
+greet_with_default("Sunny")
 
 def add(num1, num2):
-    pass
+    return(num1+num2)
+print(add)
 
 def halve(number):
-    pass
+    return number/2
+print(halve(4))
+
